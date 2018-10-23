6c0954617f707e61947d2d7d7ac72ebc492f4af5
kola-samuel <samuel.dev.dba@gmail.com>
2018-10-23 09:41:13 +0100

    Provider Payment Actor Service changes

---
+++ src/SFA.DAS.Payments.ProviderPayments.Model/ProviderPeriodicPayment.cs
@@ -0,0 +1,36 @@
+﻿using SFA.DAS.Payments.Model.Core;
+using System;
+using SFA.DAS.Payments.Model.Core.OnProgramme;
+using SFA.DAS.Payments.FundingSource.Model.Enum;
+
+namespace SFA.DAS.Payments.ProviderPayments.Model
+{
+    public class ProviderPeriodicPayment
+    {
+        public string PriceEpisodeIdentifier { get; set; }
+
+        public decimal AmountDue { get; set; }
+
+        public CalendarPeriod CollectionPeriod { get; set; }
+
+        public CalendarPeriod DeliveryPeriod { get; set; }
+
+        public DateTimeOffset EventTime { get; set; }
+
+        public long Ukprn { get; set; }
+
+        public Learner Learner { get; set; }
+
+        public LearningAim LearningAim { get; set; }
+
+        public string JobId { get; set; }
+
+        public byte ContractType { get; set; }
+
+        public decimal SfaContributionPercentage { get; set; }
+
+        public OnProgrammeEarningType OnProgrammeEarningType { get; set; }
+
+        public FundingSourceType FundingSourceType { get; set; }
+    }
+}
